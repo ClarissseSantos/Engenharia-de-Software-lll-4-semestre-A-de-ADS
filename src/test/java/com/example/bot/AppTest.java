af563d7abf9c8188b19bbb89d1bcda844bd843db
Pedro Minicz <pedrominicz@gmail.com>
2020-09-26 12:13:46 -0300

    Second bot: echo bot on Spring.

---
+++ src/test/java/com/example/bot/AppTest.java
@@ -0,0 +1,13 @@
+package com.example.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
