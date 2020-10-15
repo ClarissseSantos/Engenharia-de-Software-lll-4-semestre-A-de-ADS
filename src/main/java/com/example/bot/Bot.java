c8c5ddbb229a009f22b3a563694598aaaa7749cd
Pedro Minicz <pedrominicz@gmail.com>
2020-10-15 14:04:01 -0300

    Add reply keyboard.

--- src/main/java/com/example/bot/Bot.java
+++ src/main/java/com/example/bot/Bot.java
@@ -1,9 +1,13 @@
 package com.example.bot;
 
+import java.util.Collections;
+
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
@@ -12,9 +16,20 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(final Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
+            final KeyboardRow keyboard = new KeyboardRow();
+            keyboard.add("Again");
+            keyboard.add("Hard");
+            keyboard.add("Good");
+            keyboard.add("Easy");
+
+            final ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(Collections.singletonList(keyboard))
+                    .setResizeKeyboard(true);
+
             final SendMessage message = new SendMessage()
                     .setChatId(update.getMessage().getChatId())
-                    .setText(update.getMessage().getText());
+                    .setText(update.getMessage().getText())
+                    .setReplyMarkup(replyKeyboardMarkup);
+
             try {
                 execute(message);
             } catch (final TelegramApiException e) {
