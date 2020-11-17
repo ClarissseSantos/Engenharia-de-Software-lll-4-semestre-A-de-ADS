ae975103aa6a6dda77a8c78639d14062912c8a8f
Cleber Silva <cleber.silva46@fatec.sp.gov.br>
2020-11-16 21:18:51 -0300

    Inicializa banco.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO question (question, answer, wrong_answer1, wrong_answer2, wrong_answer3)
+VALUES ('Quem fundou a escola Jônica?', 'Tales', 'Anaximandro', 'Anaxímenes', 'Heráclito');
+
+INSERT INTO question (question, answer, wrong_answer1, wrong_answer2, wrong_answer3)
+VALUES ('Quem é considerado o pai da dialética?', 'Heráclito', 'Tales', 'Platão', 'Anaxímenes');
+
+INSERT INTO question (question, answer, wrong_answer1, wrong_answer2, wrong_answer3)
+VALUES ('Quem afirmou que "não se pode percorrer duas vezes o mesmo rio"?', 'Heráclito', 'Sócrates', 'Platão', 'Anaxímenes');
